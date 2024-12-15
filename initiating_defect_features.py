19990c198f5ecec4600c64ad60ec43ada0c87a1b
Anthonye <aml334@case.edu>
2024-12-15 13:54:14 -0500

    added regression to initiating defect features

--- initiating_defect_features.py
+++ initiating_defect_features.py
@@ -8,7 +8,13 @@ import matplotlib.pyplot as plt
 import ast
 from joblib import Parallel, delayed
 import seaborn as sns
-import scipy.stats 
+# import scipy
+import sklearn.linear_model
+import sklearn.metrics
+import sklearn.model_selection
+import sklearn.preprocessing
+import sklearn.pipeline
+import itertools
 
 combined_df = pd.read_csv('/mnt/vstor/CSE_MSE_RXF131/lab-staging/mds3/AdvManu/fractography/combined_df.csv')
 points_df = combined_df[~combined_df['points'].isna()]
@@ -111,54 +117,207 @@ def calculate_aspect_ratio_rotated(binary_image):
     return aspect_ratio
 
 # %%
-if __name=="main":
+'''Loading Images and Polygons'''
+imgs = []
+energy = []
+stress = []
+laser_power = []
+laser_speed = []
+cycles = []
+print(points_df.columns)
+for row in points_df.iterrows():
+    imgs.append(
+        extract_largest_object(
+            cv2.imread(row[1]['image_path']),
+            ast.literal_eval(row[1]['points'])
+            )
+    )
+    laser_power.append(row[1]['scan_power_W'])
+    laser_speed.append(row[1]['scan_velocity_mm_s'])
+    energy.append(row[1]['energy_density_J_mm3'])
+    stress.append(row[1]['test_stress_Mpa'])
+    cycles.append(row[1]['cycles'])
 # %%
-    '''Loading Images and Polygons'''
-    imgs = []
-    energy = []
-    stress = []
-    print(points_df.columns)
-    for row in points_df.iterrows():
-        imgs.append(
-            extract_largest_object(
-                cv2.imread(row[1]['image_path']),
-                ast.literal_eval(row[1]['points'])
-                )
-        )
-        energy.append(row[1]['energy_density_J_mm3'])
-        stress.append(row[1]['test_stress_Mpa'])
-    # %%
-    portion_of_screen = Parallel(n_jobs=-1)(delayed(lambda x: x.sum() / (x.size * x.max()))(x) for x in imgs)
-    max_sharpness = Parallel(n_jobs=-1)(delayed(find_sharpness)(x) for x in imgs)
-    aspect_ratio = Parallel(n_jobs=-1)(delayed(calculate_aspect_ratio_rotated)(x) for x in imgs)
-    perimeter = Parallel(n_jobs=-1)(delayed(get_perimeter)(x) for x in imgs)
-    pixels = Parallel(n_jobs=-1)(delayed(lambda x: x.sum() / x.max())(x) for x in imgs)
-    pixel_perimeter_ratio = Parallel(n_jobs=-1)(delayed(lambda x: x.sum() / (x.max() * get_perimeter(x)))(x) for x in imgs)
-
-    data = {
-        "screen_portion": portion_of_screen,
-        "max_sharpness": max_sharpness,
-        "aspect_ratio": aspect_ratio,
-        "perimeter": perimeter,
-        "pixels": pixels,
-        "pixel_perimeter_ratio": pixel_perimeter_ratio,
-        "energy_density":energy,
-        "stress Mpa":stress
-    }
-    df = pd.DataFrame(data).replace([np.inf, -np.inf], np.nan).dropna()
-
-    def annotate_r2(x, y, **kwargs):
-        slope, intercept, r_value, p_value, std_err = scipy.stats.linregress(x, y)
-        r_squared = r_value ** 2
-        ax = plt.gca()
-        ax.annotate(f"$R^2$ = {r_squared:.2f}", xy=(0.05, 0.9), xycoords=ax.transAxes, fontsize=10)
-
-    # Create pairplot with linear regression
-    g = sns.pairplot(df, kind="reg", plot_kws={"line_kws": {"color": "red"}})
-
-    # Add R-squared annotations to each plot
-    g.map(annotate_r2)
-    plt.show()
+'''Extracting Features'''
+portion_of_screen = Parallel(n_jobs=-1)(delayed(lambda x: x.sum() / (x.size * x.max()))(x) for x in imgs)
+max_sharpness = Parallel(n_jobs=-1)(delayed(find_sharpness)(x) for x in imgs)
+aspect_ratio = Parallel(n_jobs=-1)(delayed(calculate_aspect_ratio_rotated)(x) for x in imgs)
+perimeter = Parallel(n_jobs=-1)(delayed(get_perimeter)(x) for x in imgs)
+pixels = Parallel(n_jobs=-1)(delayed(lambda x: x.sum() / x.max())(x) for x in imgs)
+pixel_perimeter_ratio = Parallel(n_jobs=-1)(delayed(lambda x: x.sum() / (x.max() * get_perimeter(x)))(x) for x in imgs)
+data = {
+    "screen_portion": portion_of_screen,
+    "max_sharpness": max_sharpness,
+    "aspect_ratio": aspect_ratio,
+    "perimeter": perimeter,
+    "pixels": pixels,
+    "pixel_perimeter_ratio": pixel_perimeter_ratio,
+    "energy_density":energy,
+    "stress_Mpa":stress,
+    "laser_power":laser_power,
+    "laser_speed":laser_speed,
+    "cycles": cycles
+}
+df = pd.DataFrame(data).replace([np.inf, -np.inf], np.nan).dropna()
 # %%
-else:
-    print(__name__+" function loaded")
+'''Plotting Features'''
+def annotate_r2(x, y, **kwargs):
+    x = np.array(x).reshape(-1, 1)
+    y = np.array(y)
+    model = sklearn.linear_model.LinearRegression()
+    model.fit(x, y)
+    r_squared = model.score(x, y)
+    ax = plt.gca()
+    ax.annotate(f"$R^2$ = {r_squared:.2f}", xy=(0.05, 0.9), xycoords=ax.transAxes, fontsize=10)
+
+# Create pairplot with linear regression
+g = sns.pairplot(df, kind="reg", plot_kws={"line_kws": {"color": "red"}})
+
+# Add R-squared annotations to each plot
+g = g.map(annotate_r2)
+
+# %%
+'''Evaluate Predictors'''
+def multiple_linear_regression(X,Y):
+    # Sklearn Linear Regression
+    X_train, X_test, y_train, y_test = sklearn.model_selection.train_test_split(X, Y, test_size=0.2, random_state=42)
+    
+    # Scale features
+    scaler = sklearn.preprocessing.StandardScaler()
+    X_train_scaled = scaler.fit_transform(X_train)
+    X_test_scaled = scaler.transform(X_test)
+    
+    # Fit model
+    lr = sklearn.linear_model.LinearRegression()
+    lr.fit(X_train_scaled, y_train)
+    
+    # Predict and evaluate
+    y_pred = lr.predict(X_test_scaled)
+    mse = sklearn.metrics.mean_squared_error(y_test, y_pred)
+    r2 = sklearn.metrics.r2_score(y_test, y_pred)
+    return lr, y_pred, mse, r2
+
+def polynomial_regression(X,Y,degree=3):
+    X_train, X_test, y_train, y_test = sklearn.model_selection.train_test_split(X, Y, test_size=0.2, random_state=42)
+    
+    # Create polynomial pipeline
+    poly_model = sklearn.pipeline.make_pipeline(
+        sklearn.preprocessing.StandardScaler(),
+        sklearn.preprocessing.PolynomialFeatures(degree),
+        sklearn.linear_model.LinearRegression()
+    )
+
+    # Fit model
+    poly_model.fit(X_train, y_train)
+
+    # Predict and evaluate
+    y_pred = poly_model.predict(X_test)
+    mse = sklearn.metrics.mean_squared_error(y_test, y_pred)
+    r2 = sklearn.metrics.r2_score(y_test, y_pred)
+    return poly_model, y_pred, mse, r2
+
+# 3. Ridge Regression
+def ridge_regression(X,Y):
+    X_train, X_test, y_train, y_test = sklearn.model_selection.train_test_split(X, Y, test_size=0.2, random_state=42)
+    
+    # Scale features
+    scaler = sklearn.preprocessing.StandardScaler()
+    X_train_scaled = scaler.fit_transform(X_train)
+    X_test_scaled = scaler.transform(X_test)
+        
+    # Ridge Regression
+    ridge = sklearn.linear_model.Ridge(alpha=1.0)
+    ridge.fit(X_train_scaled, y_train)
+
+    # Predict and evaluate
+    y_pred = ridge.predict(X_test_scaled)
+    mse = sklearn.metrics.mean_squared_error(y_test, y_pred)
+    r2 = sklearn.metrics.r2_score(y_test, y_pred)
+    return ridge, y_pred, mse, r2
+
+# 4. Lasso Regression
+def lasso_regression(X,Y):
+    X_train, X_test, y_train, y_test = sklearn.model_selection.train_test_split(X, Y, test_size=0.2, random_state=42)
+    
+    # Scale features
+    scaler = sklearn.preprocessing.StandardScaler()
+    X_train_scaled = scaler.fit_transform(X_train)
+    X_test_scaled = scaler.transform(X_test)
+        
+    # Lasso Regression
+    lasso = sklearn.linear_model.Lasso(alpha=0.1)
+    lasso.fit(X_train_scaled, y_train)
+
+    # Predict and evaluate
+    y_pred = lasso.predict(X_test_scaled)
+    mse = sklearn.metrics.mean_squared_error(y_test, y_pred)
+    r2 = sklearn.metrics.r2_score(y_test, y_pred)
+    return lasso, y_pred, mse, r2
+
+metric_used_list = []
+regression_type = []
+energy_density_list = []
+laser_power_list = []
+scan_speed_list = []
+aspect_ratio_list = []
+sharpness_list = []
+y_pred_list = []
+mse_list = []
+r2_list = []
+for metric in ['log_stress','stress']:
+    for regression in [multiple_linear_regression,polynomial_regression,ridge_regression,lasso_regression]:
+        for inputs in list(itertools.product([True, False], repeat=5)):
+            if True in inputs: # There needs to be at least 1 predictor
+                regression_type.append(regression.__name__)
+                inputs_columns = []
+                if inputs[0]:
+                    energy_density_list.append(True)
+                    inputs_columns.append('energy_density')
+                else:
+                    energy_density_list.append(False)
+                if inputs[1]:
+                    laser_power_list.append(True)
+                    inputs_columns.append('laser_power')
+                else:
+                    laser_power_list.append(False)
+                if inputs[2]:
+                    scan_speed_list.append(True)
+                    inputs_columns.append('laser_speed')
+                else:
+                    scan_speed_list.append(False)
+                if inputs[3]:
+                    aspect_ratio_list.append(True)
+                    inputs_columns.append('aspect_ratio')
+                else:
+                    aspect_ratio_list.append(False)
+                if inputs[4]:
+                    sharpness_list.append(True)
+                    inputs_columns.append('max_sharpness')
+                else:
+                    sharpness_list.append(False)
+                X = df[inputs_columns]
+                if metric =='log_stress':
+                    Y = df['cycles'].apply(math.log)
+                    metric_used_list.append('log(stress)')
+                elif metric =='stress':
+                    Y = df['cycles'].apply(math.log)*df['stress_Mpa']
+                    metric_used_list.append('stress')
+                model, y_pred, mse, r2 = regression(X,Y)
+                mse_list.append(mse)
+                r2_list.append(r2)
+
+regression_dict = {
+    "metric_used":metric_used_list,
+    "regression_type":regression_type,
+    "energy_density":energy_density_list,
+    "laser_power":laser_power_list,
+    "scan_speed":scan_speed_list,
+    "aspect_ratio":aspect_ratio_list,
+    "sharpness":sharpness_list,
+    "mse":mse_list,
+    "r2":r2_list
+}
+df_results = pd.DataFrame(regression_dict)
+
+print(df_results.iloc[df_results['r2'].idxmax()])
+print(df_results.iloc[df_results[(df_results['aspect_ratio']==False) & (df_results['sharpness']==False)]['r2'].idxmax()])
